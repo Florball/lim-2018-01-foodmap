12ec783e9e6cb482903272b9c67459028e51d265
Florball <flor_ballinas@hotmail.com>
2018-08-16 16:23:43 -0500

    guardando evento buscar por input

--- parallax-template/js/app.js
+++ parallax-template/js/app.js
@@ -5,38 +5,30 @@ const searchRestaurant = document.getElementById('icon_prefix');
 // Boton para buscar
 const search = document.getElementById('search');
 //Contenedor de restaurantes
-
 let containerOfRestaurant = document.getElementById('list-of-restaurants')
+
 //falta agregar foto de restaurante
-// Recorrer
+// Restaurante por distrito
 const getRestaurant = (value) => {
   containerOfRestaurant.innerHTML='';
-  for (let restaurant of restaurants) {
-    if (restaurant.district === value) {
+  for (let key of restaurants) {
+    if (key.district === value) {
       // Contenido pintado
     containerOfRestaurant.innerHTML += `<div class="col center s12">
     <ul>
-      <li>${restaurant.name}</li>
+      <li>${key.name}</li>
     </ul>
   </div>`;
     }; 
   };
 };
 
-// Filtra por select
+// Evento select
 districtSelect.addEventListener('change', (e) => {
-  getRestaurant(e.target.value)
+  getRestaurant(e.target.value);
 });
 
-
-
-
-//  const list = restaurantesBarranquinos.filter((barranco) => {
-//   let nombre = barranco.name.toUpperCase();
-//   nombre = nombre.indexOf(search.toUpperCase()) + 1;
-//   return nombre;
-// });
-// //  funcion de evento keyup para filtrar
-// searchRestaurant.addEventListener('keyup', (event) => { // El evento keyup en un addevEventListener ocurre cuando el usuario suelta una tecla (en el teclado)
-//   filter(event.target.value); 
-// });
+// Evento keyup 
+searchRestaurant.addEventListener('keyup', (e) => { // El evento keyup en un addevEventListener ocurre cuando el usuario suelta una tecla (en el teclado)
+  getRestaurant(event.target.value); 
+});
