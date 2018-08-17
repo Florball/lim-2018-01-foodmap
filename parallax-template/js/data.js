220fb579d3f3a5651a471244046ec3a872c78ace
Florball <flor_ballinas@hotmail.com>
2018-08-16 20:23:59 -0500

    cambiando nombres de restaurantes a mayúsculas

--- parallax-template/js/data.js
+++ parallax-template/js/data.js
@@ -1,70 +1,70 @@
 const restaurants = [
   {
     "district": "BARRANCO",
-    "name": "Isolina",
+    "name": "ISOLINA",
     "address": "Av San Martin 101, Lima 15063",
     "web": "isolina.pe",
     "cost": 'Costo aproximado por plato 25'
   },
   {
     "district": "BARRANCO",
-    "name": "El muelle cevicheria",
+    "name": "EL MUELLE CEVICHERIA",
     "address": "Jr. Alfonso Ugarte 206, Lima 15063",
     "web": "cevicheriaelmuelle.com",
     "cost": 'Costo aproximado por plato 20'
   },
   {
     "district": "BARRANCO",
-    "name": "La linterna",
+    "name": "LA LINTERNA",
     "address": "Av. Almte. Miguel Grau 689",
     "web": "pizzerialalinterna.com",
     "cost": 'Costo aproximado por plato 40'
   },
   {
     "district": "BARRANCO",
-    "name": "Amoramar",
+    "name": "AMORAMAR",
     "address": "Av. Aviación 4555,",
     "web": "amoramar.com",
     "cost": 'Costo aproximado por plato 35'
   },
   {
     "district": "BARRANCO",
-    "name": "Cala",
+    "name": "CALA",
     "address": "Circuito de Playas,",
     "web": "calanamee.com",
     "cost": 'Costo aproximado por plato 50'
   },
   {
     "district": "SURCO",
-    "name": "Tierra Santa",
+    "name": "TIERRA SANTA",
     "address": "Av. Caminos del Inca 15129",
     "web": "nametierrasanta.com",
     "cost": 'Costo aproximado por plato 15'
   },
   {
     "district": "SURCO",
-    "name": "Embarcadero 41",
+    "name": "EMBARCADERO 41",
     "address": "Calle Alexander Fleming 181",
     "web": "embarcadero41.com",
     "cost": 'Costo aproximado por plato 30'
   },
   {
     "district": "SURCO",
-    "name": "Jose Antonio",
+    "name": "JOSE ANTONIO",
     "address": "Trinidad 320, Distrito de Lima 15037",
     "web": "joseantonio.com.pe",
     "cost": 'Costo aproximado por plato 25'
   },
   {
     "district": "SURCO",
-    "name": "Rodizio",
+    "name": "RODIZIO",
     "address": "Av. Aviación 4555",
     "web": "rodizio.com.pe",
     "cost": 'Costo aproximado por plato 25'
   },
   {
     "district": "SURCO",
-    "name": "Choy Tac",
+    "name": "CHOY TAC",
     "address": "Avenida Alfredo Benavides Diez Canseco 3801",
     "web": "nameechoytac.com",
     "cost": 'Costo aproximado por plato 20'
